c6b9d9d1f7d305dd9bc602152a1cc6a4e57a0c6d
Erick Gutierrez <152904782+MrErick18@users.noreply.github.com>
2024-08-27 11:03:49 -0500

    funcionalidad de menu de ingreso al voto

--- MEAN/client/src/app/components/menu-voto/menu-voto.component.ts
+++ MEAN/client/src/app/components/menu-voto/menu-voto.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-menu-voto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Importa FormsModule aquí
   templateUrl: './menu-voto.component.html',
-  styleUrl: './menu-voto.component.css'
+  styleUrls: ['./menu-voto.component.css']
 })
 export class MenuVotoComponent {
+  tipoDoc: string = '';
+  numDoc: string = '';
 
+  constructor(private usuariosService: UsuariosService, private toastr: ToastrService, private router: Router) {}
+
+  onSubmit() {
+    this.usuariosService.validarUsuario(this.tipoDoc, this.numDoc).subscribe(
+      (response) => {
+        if (response && response._id) {
+          this.router.navigate(['/ingreso-voto']); // Cambia la ruta según tu necesidad
+        } else {
+          this.toastr.warning('Usuario no encontrado.');
+        }
+      },
+      (error) => {
+        this.toastr.error('Ocurrió un error al validar el usuario.');
+      }
+    );
+  }
 }
